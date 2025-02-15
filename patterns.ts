67681eb71b879d82cd3921d333a99d70732080e5
Tero Lehtonen <terol@kapsi.fi>
2025-02-15 23:03:26 +0200

    Add stuff

--- patterns.ts
+++ patterns.ts
@@ -29,32 +29,61 @@ type Card = {
   count: Count;
 }
 
+function cardsEqual(a: Card, b: Card): boolean {
+  return a.color === b.color && a.shape === b.shape && a.filling === b.filling && a.count === b.count;
+}
+
+class GameState {
+  foundSets: Card[][];
+
+  constructor() {
+    this.foundSets = [];
+  };
+
+  isFound(card1: Card, card2: Card, card3: Card): boolean {
+    const sorted = [card1, card2, card3].sort();
+    return this.foundSets.some((found) => {
+      return cardsEqual(found[0], sorted[0])
+        && cardsEqual(found[1], sorted[1])
+        && cardsEqual(found[2], sorted[2]);
+    });
+  }
+
+  addFound(card1: Card, card2: Card, card3: Card): void {
+    const sorted = [card1, card2, card3].sort();
+    if (this.isFound(card1, card2, card3)) {
+      throw "Trying to add an already found set";
+    }
+    this.foundSets.push(sorted);
+  }
+}
+
+
 function card_to_string(card: Card): string {
   return `${Color[card.color]} ${Shape[card.shape]} ${Filling[card.filling]} ${Count[card.count]}`;
 }
 
-const allCards: Card[] = (() => {
-  let cards: Card[] = [];
-  for (const color of Object.values(Color)
-    .filter((e) => isNaN(Number(e)))
-    .map((e) => Color[e as keyof typeof Color])) {
-    for (const shape of Object.values(Shape)
-      .filter((e) => isNaN(Number(e)))
-      .map((e) => Shape[e as keyof typeof Shape])) {
-      for (const filling of Object.values(Filling)
-        .filter((e) => isNaN(Number(e)))
-        .map((e) => Filling[e as keyof typeof Filling])) {
-        for (const count of Object.values(Count)
-          .filter((e) => isNaN(Number(e)))
-          .map((e) => Count[e as keyof typeof Count])) {
-          cards.push({ color: color, shape: shape, filling: filling, count: count });
-        }
-      }
-    }
-  }
-  return cards;
-})();
 
+function is_set(card1: Card, card2: Card, card3: Card): boolean {
+  const color_set = new Set([card1.color, card2.color, card3.color]);
+  if (color_set.size === 2) {
+    return false;
+  }
+  const shape_set = new Set([card1.shape, card2.shape, card3.shape]);
+  if (shape_set.size === 2) {
+    return false;
+  }
+  const filling_set = new Set([card1.filling, card2.filling, card3.filling]);
+  if (filling_set.size === 2) {
+    return false;
+  }
+  const count_set = new Set([card1.count, card2.count, card3.count]);
+  if (count_set.size === 2) {
+    return false;
+  }
+
+  return true;
+}
 function find_sets(cards: Card[]): Card[][] {
   let sets: Card[][] = [];
 
@@ -65,24 +94,9 @@ function find_sets(cards: Card[]): Card[][] {
         const card2 = cards[card2_i];
         const card3 = cards[card3_i];
 
-        const color_set = new Set([card1.color, card2.color, card3.color]);
-        if (color_set.size === 2) {
-          continue;
+        if (is_set(card1, card2, card3)) {
+          sets.push([card1, card2, card3]);
         }
-        const shape_set = new Set([card1.shape, card2.shape, card3.shape]);
-        if (shape_set.size === 2) {
-          continue;
-        }
-        const filling_set = new Set([card1.filling, card2.filling, card3.filling]);
-        if (filling_set.size === 2) {
-          continue;
-        }
-        const count_set = new Set([card1.count, card2.count, card3.count]);
-        if (count_set.size === 2) {
-          continue;
-        }
-
-        sets.push([card1, card2, card3]);
       }
     }
   }
@@ -120,16 +134,67 @@ function new_set_of_6(all_cards: Card[]): Card[] {
   return found;
 }
 
-let current_set = new_set_of_6(allCards);
+let currentGameState: GameState = new GameState();
 
-const btns = Array.from(document.getElementsByClassName("card"));
+const allCards: Card[] = (() => {
+  let cards: Card[] = [];
+  for (const color of Object.values(Color)
+    .filter((e) => isNaN(Number(e)))
+    .map((e) => Color[e as keyof typeof Color])) {
+    for (const shape of Object.values(Shape)
+      .filter((e) => isNaN(Number(e)))
+      .map((e) => Shape[e as keyof typeof Shape])) {
+      for (const filling of Object.values(Filling)
+        .filter((e) => isNaN(Number(e)))
+        .map((e) => Filling[e as keyof typeof Filling])) {
+        for (const count of Object.values(Count)
+          .filter((e) => isNaN(Number(e)))
+          .map((e) => Count[e as keyof typeof Count])) {
+          cards.push({ color: color, shape: shape, filling: filling, count: count });
+        }
+      }
+    }
+  }
+  return cards;
+})();
 
-const zipped: [Card, Element][] = current_set.map((a, i) => [a, btns[i]]);
-zipped.forEach(([card, element]) => {
-  let elem = element as HTMLElement;
-  elem.textContent = card_to_string(card);
-  elem.onclick = () => {
-    console.log(elem.id);
-  };
-});
+function main() {
+  let current_set = new_set_of_6(allCards);
 
+  const card_elements = Array.from(document.getElementsByClassName("card"));
+
+  const zipped: [Card, Element][] = current_set.map((a, i) => [a, card_elements[i]]);
+  zipped.forEach(([card, element]) => {
+    let elem = element as HTMLElement;
+    elem.textContent = card_to_string(card);
+    elem.setAttribute("data-card", JSON.stringify(card))
+
+    elem.onclick = (ev) => {
+
+      element.classList.toggle("card-selected");
+
+      const selected_card_elements = Array.from(document.getElementsByClassName("card-selected"));
+      console.log(selected_card_elements);
+      if (selected_card_elements.length === 3) {
+        const selected1: Card = JSON.parse(selected_card_elements[0].getAttribute("data-card") || "");
+        const selected2: Card = JSON.parse(selected_card_elements[1].getAttribute("data-card") || "");
+        const selected3: Card = JSON.parse(selected_card_elements[2].getAttribute("data-card") || "");
+
+        if (is_set(selected1, selected2, selected3)) {
+          if (!currentGameState.isFound(selected1, selected2, selected3)) {
+            currentGameState.addFound(selected1, selected2, selected3);
+            console.log("Found a set!");
+          }
+          else {
+            console.log("Set already found");
+          }
+        }
+
+        selected_card_elements.forEach((e) => { e.classList.toggle("card-selected", false) });
+
+      }
+    };
+  });
+}
+
+main();
